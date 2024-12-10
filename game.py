c28385e74d60f2615a5565911e338e2b4466b4c3
JLamb06 <jcwl2@kent.ac.uk>
2024-12-10 20:31:35 +0000

    part 2

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+import re
+import cowsay
+
 class Character:
     types = ["knight","wizard","archer"]
     avaiweapons = ["Sword","Shield","2 Handed Sword","2 Swords","Spear","Wand","Staff","Magical Stick","Bare Bow","Recurve Bow","Compound Bow"]
@@ -14,22 +17,23 @@ class Character:
 
     def main(self):
         character = self.get_character()
-        print(f"You are {character[0]} a {character[2]} with a {character[1]}")
+        print(f"You are {character.name} a {character.type} with a {character.weapon}")
 
     def get_character(self):
         name = input("Enter your characters name: ")
-        type = input(f"Enter you character type from the list {self.types}: ")
+        type = input(f"Enter you character class from the list {self.types}: ")
         type = type.lower().strip()
-        weapon = ""
         while type not in self.types:
+            type = input(f"Enter you character type from the list {self.types}: ")
+            type = type.lower().strip()
             if type == "knight":
-                weapon == avaiweapons[0]
+                weapon = self.avaiweapons[0]
             if type == "wizard":
-                weapon == avaiweapons[7]
+                weapon = self.avaiweapons[7]
             if type == "archer":
-                weapon == avaiweapons[8]
+                weapon = self.avaiweapons[8]
             else:
-                raise ValueError ("That is not a valid type")
+                print("That is not a valid class, please try again.")
         
         character = Character(name,weapon,type)
         return character
@@ -37,6 +41,7 @@ class Character:
 if __name__ == "__main__":
     character = Character("", "","")
     character.main()
+
 
 
 #Weapon = sword & shield, bow, sword, 2 handed sword, 2 swords, spear, wand,staff, magical stick
